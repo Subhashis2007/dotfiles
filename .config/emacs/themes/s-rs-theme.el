27a4852c0f6a228f2f57d1f4a1e281eee8e1979f
subhashis <baradsubhashis@gmail.com>
2023-08-17 19:55:23 +0530

    change emacs theme

---
+++ .config/emacs/themes/s-rs-theme.el
@@ -0,0 +1,37 @@
+;;; s-rs-theme.el --- s-rs
+;;; Version: 1.0
+;;; Commentary:
+;;; A theme called s-rs
+;;; Code:
+
+(deftheme s-rs "DOCSTRING for s-rs")
+  (custom-theme-set-faces 's-rs
+   '(default ((t (:foreground "#e1e1e1" :background "#1c1e33" ))))
+   '(cursor ((t (:background "#ffeb7a" ))))
+   '(fringe ((t (:background "#2d2f45" ))))
+   '(mode-line ((t (:foreground "#ffffff" :background "#0e192d" ))))
+   '(region ((t (:background "#bb089f" ))))
+   '(secondary-selection ((t (:background "#ff74fb" ))))
+   '(font-lock-builtin-face ((t (:foreground "#ffb400" ))))
+   '(font-lock-comment-face ((t (:foreground "#7c6f64" ))))
+   '(font-lock-function-name-face ((t (:foreground "#b8bb26" ))))
+   '(font-lock-keyword-face ((t (:foreground "#bb34fb" ))))
+   '(font-lock-string-face ((t (:foreground "#64f7b0" ))))
+   '(font-lock-type-face ((t (:foreground "#009aff" ))))
+   '(font-lock-constant-face ((t (:foreground "#90d3ff" ))))
+   '(font-lock-variable-name-face ((t (:foreground "#32ff02" ))))
+   '(minibuffer-prompt ((t (:foreground "#b8bb26" :bold t ))))
+   '(font-lock-warning-face ((t (:foreground "red" :bold t ))))
+   )
+
+;;;###autoload
+(and load-file-name
+    (boundp 'custom-theme-load-path)
+    (add-to-list 'custom-theme-load-path
+                 (file-name-as-directory
+                  (file-name-directory load-file-name))))
+;; Automatically add this theme to the load path
+
+(provide-theme 's-rs)
+
+;;; s-rs-theme.el ends here
