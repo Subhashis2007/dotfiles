986a4451db815895c36c0fbf7285a5d2e21615dd
subhashis2007 <subhashis2007@skiff.com>
2024-01-27 22:52:21 +0530

    config: nvim: migrate to Lazy

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,10 +1,13 @@
-function BringMeColors(color)
-        color = color or "tokyonight-night"
-	vim.cmd.colorscheme(color)
-        
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-           
+function ColorMyPencils(color)
+  color = color or "tokyonight-night"
+  local transparent = false
 
-BringMeColors()
+  vim.cmd.colorscheme(color)
+
+  if (transparent) then
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- 0 means global
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- 0 means global
+  end
+end
+
+ColorMyPencils()
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,18 @@
-require("user")
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
 
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
+vim.opt.rtp:prepend(lazypath)
 
--- empty setup using defaults
-require("nvim-tree").setup()
+vim.g.mapleader = " "
 
--- OR setup with some options
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+require("lazy").setup("plugins")
+require("smooth")
