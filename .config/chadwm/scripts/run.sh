d777eca8971283783eeee89aef5a1f441bc45140
subhashis2007 <subhashis2007@skiff.com>
2024-01-27 10:20:46 +0530

    config: chadwm: scripts: refactor run.sh and clean up

--- .config/chadwm/scripts/run.sh
+++ .config/chadwm/scripts/run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-xrdb merge ~/.Xresources 
-xset r rate 200 50 &
-picom &
-/usr/bin/emacs --daemon &
-# nitrogen --restore &
-variety &
-xscreensaver -no-splash &
+xset r rate 200 75 &
+picom -b  --config ~/.config/chadwm/picom/picom.conf &
+nitrogen --restore &
+volumeicon &
+run "numlockx on"
 run "bash ~/.config/chadwm/scripts/settheme"
 run "bash ~/.config/chadwm/scripts/startup"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 dash ~/.config/chadwm/scripts/bar.sh &
+nm-applet &
 while type chadwm >/dev/null; do chadwm && continue || break; done
