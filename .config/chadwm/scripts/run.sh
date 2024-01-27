9ee2d88ec52e6ee917367508de86784bc72e73f6
subhashis2007 <subhashis2007@skiff.com>
2024-01-27 12:17:32 +0530

    config: chadwm: scripts: refactor a little, (literally a little)

--- .config/chadwm/scripts/run.sh
+++ .config/chadwm/scripts/run.sh
@@ -4,10 +4,10 @@ xset r rate 200 75 &
 picom -b  --config ~/.config/chadwm/picom/picom.conf &
 nitrogen --restore &
 volumeicon &
+nm-applet &
 run "numlockx on"
 run "bash ~/.config/chadwm/scripts/settheme"
 run "bash ~/.config/chadwm/scripts/startup"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 dash ~/.config/chadwm/scripts/bar.sh &
-nm-applet &
 while type chadwm >/dev/null; do chadwm && continue || break; done
