4a0378232fc22e5757bb13ab2980fe5f6f458359
subhashis2007 <subhashis2007@skiff.com>
2024-01-28 20:25:26 +0530

    config: nvim: after: plugin: telescope: set to show hidden files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,17 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
+-- Import necessary modules
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
+telescope.setup({
+  pickers = {
+    find_files = {
+      hidden = true, 
+    },
+  },
+})
+
+
+
