e3730cfe3c9774333a7af83f18849c2c69eb451f
subhashis2007 <subhashis2007@skiff.com>
2024-01-30 10:12:43 +0530

    config: chadwm: scripts: run.sh: remove pa-applet

--- .config/chadwm/scripts/run.sh
+++ .config/chadwm/scripts/run.sh
@@ -5,7 +5,6 @@ picom -b  --config ~/.config/chadwm/picom/picom.conf &
 dash ~/.config/chadwm/scripts/bar.sh &
 nitrogen --restore &
 nm-applet &
-pa-applet &
 run "numlockx on"
 run "bash ~/.config/chadwm/scripts/settheme"
 run "bash ~/.config/chadwm/scripts/startup"
